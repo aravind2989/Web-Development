b1a730cd63ac75a1340c9c5fb26dbd7247c36fa7
rishi <rushi@gmail.com>
2024-05-10 01:32:41 +0530

    ....Be

---
+++ project-2/script.js
@@ -0,0 +1,20 @@
+gsap.from(".img1",{
+    opacity:0,
+    delay:0.4,
+    duration:1,
+    y:60
+})
+
+gsap.from(".img2",{
+    opacity:0,
+    delay:0.4,
+    duration:1,
+    x:60
+})
+
+gsap.from(".img3",{
+    opacity:0,
+    delay:0.4,
+    duration:1,
+    x:-60
+})
